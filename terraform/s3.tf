3c852e7a0814588e70e9860c28d05983814dfb95
itsuki <miumosh@gmail.com>
2020-02-20 15:17:39 +0900

    add s3 #5

---
+++ terraform/s3.tf
@@ -0,0 +1,27 @@
+# ------------------------------
+#  S3 for Web-img
+# ------------------------------
+
+# # s3 for web-img
+resource "aws_s3_bucket" "web_img" {
+  bucket = "taskleaf-web-img"
+  region = "ap-northeast-1"
+}
+
+# policy for alb-log-s3
+resource "aws_bucket_policy" "web_img" {
+  bucket = aws_s3_bucket.web_img.id
+  policy = data.aws_iam_policy_document.web_img.json
+}
+data "aws_iam_policy_document" "web_img" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:*"]
+    resources = ["arn:aws:s3:::aws_s3_bucket.alb_log.id/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
