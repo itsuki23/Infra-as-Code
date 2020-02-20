3c8800f57b27c22a1d597225ddca638265d5cdb3
itsuki <miumosh@gmail.com>
2020-02-20 17:42:09 +0900

    add acm #14

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,20 +8,20 @@ resource "aws_s3_bucket" "web_img" {
   region = "ap-northeast-1"
 }
 
-# policy for alb-log-s3
-resource "aws_s3_bucket_policy" "web_img" {
-  bucket = aws_s3_bucket.web_img.id
-  policy = data.aws_iam_policy_document.web_img.json
-}
-data "aws_iam_policy_document" "web_img" {
-  statement {
-    effect = "Allow"
-    actions = ["s3:*"]
-    resources = ["arn:aws:s3:::aws_s3_bucket.alb_log.id/*"]
+# # policy for alb-log-s3
+# resource "aws_s3_bucket_policy" "web_img" {
+#   bucket = aws_s3_bucket.web_img.id
+#   policy = data.aws_iam_policy_document.web_img.json
+# }
+# data "aws_iam_policy_document" "web_img" {
+#   statement {
+#     effect = "Allow"
+#     actions = ["s3:*"]
+#     resources = ["arn:aws:s3:::aws_s3_bucket.web_img.id/*"]
 
-    principals {
-      type = "AWS"
-      identifiers = ["582318560864"]
-    }
-  }
-}
+#     principals {
+#       type = "AWS"
+#       identifiers = ["582318560864"]
+#     }
+#   }
+# }
